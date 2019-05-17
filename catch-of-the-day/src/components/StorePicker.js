908a831ba2c4c4d3c8904287bc4330eeb9d6c04c
Alan Healy <alannhealy@gmail.com>
2019-05-17 16:55:13 -0700

    Added Routing

--- catch-of-the-day/src/components/StorePicker.js
+++ catch-of-the-day/src/components/StorePicker.js
@@ -1,21 +1,24 @@
 import React from 'react';
 
 class StorePicker extends React.Component {
-
+    constructor(){
+        super();
+        this.goToStore = this.goToStore.bind(this);
+        
+    }
+    goToStore(e){
+        console.log("Going to Store");
+        e.preventDefault();
+    }
     render() {
         return (
-            <React.Fragment>
-                { /* Must use a block comment */ }
-                <p>Up ya boy ya</p>
-                <form> 
-                    <h2>Please Enter a Store</h2>
-                    <input type="text" required placeholder="Store Name" />
-                    <button type="submit">Visit Store</button>
-                </form>
-            </React.Fragment>
-        )
+            <form className="store-selector" onSubmit={this.goToStore}> 
+                <h2>Please Enter a Store</h2>
+                <input type="text" required placeholder="Store Name" />
+                <button type="submit">Visit Store</button>
+            </form>
+        );
     }
-
 }
 
 export default StorePicker;
